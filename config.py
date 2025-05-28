1e6a5474d7c5dca4009ea95247fc0a49de6b42b2
Roman <panichevr@gmail.com>
2025-05-28 14:42:22 +0300

    Deleted the token from config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '7834043258:AAGQ6YGfIe-Eet7MRCIhybU5qRzjxJYZCqw'
+TOKEN = ''
 
 curr_dict = {'Евро':'EUR', 'Доллар':'USD', 'Рубль':'RUB'}
 formatted_dict = '\n'.join([f'{i+1}. {curr}' for i, curr in enumerate(curr_dict.keys())])
